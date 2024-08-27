9724664be3395752bcdfb78a58bbb93422f01a3b
Leonardo Teixeira Martins <leonardo.t@escolar.ifrn.edu.br>
2024-08-27 15:33:42 -0300

    ✅ endpoint de tarefas funcionando.

--- microsservicos/tarefas/tarefa/views.py
+++ microsservicos/tarefas/tarefa/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from models import Tarefa
+from .models import Tarefa
+from .serializers import TarefaSerializer
 from rest_framework import permissions, viewsets
 
 class TarefaViewSet(viewsets.ModelViewSet):
