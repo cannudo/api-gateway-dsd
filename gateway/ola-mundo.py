50a47c495603db403ad442ab2832df1c6d386a6e
Luan da Costa Redmann <luandacostaredmann@gmail.com>
2024-08-28 21:14:19 +0000

    :bulb: como Flask retornaria JSON?

--- gateway/ola-mundo.py
+++ gateway/ola-mundo.py
@@ -7,7 +7,8 @@ def ola_mundo():
     '''
     Isto é: execute a função ola_mundo() quando o Flask detectar uma requisição para a rota /
     '''
-    return "<h1>Olá, mundo!</h1>"
+    #return "<h1>Olá, mundo!</h1>"
+    return {"etiquetas": "http://localhost:5000/etiquetas/"} # Python converte isso para um JSON
 
 @app.route("/etiquetas/")
 def listar_etiquetas():
@@ -22,7 +23,18 @@ def testar_metodo():
     if request.method == "GET":
         retorno = "Veio um GET"
     elif request.method == "POST":
-        retorno = "Veio um post"
+        try:
+            teste = request.form['atributo_da_requisicao'] # só vai ter alguma coisa se for passado como form-data (só consegui com postman)
+        except KeyError:
+            teste = None
+        retorno = f"Veio um post ({teste})"
     else: # a requisição nunca chega nesse else, porque outros métodos retornam 405 automaticamente
         retorno = "Veio outro método"
+    return retorno
+
+@app.put("/metodos/")
+def testar_put():
+    retorno = "PUT funcionou"
+    if request.method != "PUT":
+        retorno = "Será que um GET cai aqui?" # Não. Retorna 405 automaticamente
     return retorno
