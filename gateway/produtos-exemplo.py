2b490446aeedd887f57ebff538ce0ca5fe8faa9e
Luan da Costa Redmann <luandacostaredmann@gmail.com>
2024-08-28 18:31:39 +0000

    :bulb: comenta sobre como funciona o código do gateway

--- gateway/produtos-exemplo.py
+++ gateway/produtos-exemplo.py
@@ -8,14 +8,14 @@ port = int(os.environ.get('PORT', 5000))
 def home():
     return "Olá, mundo!"
 
-BASE_URL = "https://dummyjson.com"
-@app.route('/products', methods=['GET'])
+BASE_URL = "https://dummyjson.com" # API pública
+@app.route('/products', methods=['GET']) # adiciona a rota /productts no app Flask
 def get_products():
-    response = requests.get(f"{BASE_URL}/products")
-    if response.status_code != 200:
+    response = requests.get(f"{BASE_URL}/products") # endpoint na API pública
+    if response.status_code != 200: 
         return jsonify({'error': response.json()['message']}), response.status_code
     products = []
-    for product in response.json()['products']:
+    for product in response.json()['products']: # Pegue os produtos no formato deles, trate e coloque no nosso formato
         product_data = {
             'id': product['id'],
             'title': product['title'],
