d4b982b9edfe34cb814f07716f2ef1bdfeb8b8e6
Luan da Costa Redmann <luandacostaredmann@gmail.com>
2024-09-05 01:02:29 +0000

    :construction: adapta servidor para receber a notificação do websocket cliente

--- websocket/ex1-server-asyncio.py
+++ websocket/ex1-server-asyncio.py
@@ -7,8 +7,12 @@ async def echo(websocket):
     async for message in websocket:
         await websocket.send(message)
 
+async def manipulador(canal):
+    async for mensagem in canal:
+        print(mensagem)
+
 async def main():
-    async with serve(echo, "localhost", 8765):
+    async with serve(manipulador, "localhost", 8765):
         await asyncio.get_running_loop().create_future()  # run forever
 
 asyncio.run(main())
